64239690e2cbb061aaa95a01096c0aefb8044e77
Mateusz Kłosiński <mkklosin@gmail.com>
2017-10-12 16:08:44 +0200

    More on Components - Building Custom Pipes

--- Path/Angular/Angular - Getting Started/src/app/app.module.ts
+++ Path/Angular/Angular - Getting Started/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 @NgModule({
     imports: [ 
       BrowserModule,
@@ -10,7 +11,8 @@ import { ProductListComponent } from './products/product-list.component';
     ],
     declarations: [ 
       AppComponent,
-      ProductListComponent
+      ProductListComponent,
+      ConvertToSpacesPipe
     ],
     bootstrap: [ AppComponent ]
   })
