1f435db54dab1f0b30d9b71c3f90fba7b72b3baf
Mateusz Kłosiński <mkklosin@gmail.com>
2017-10-22 23:48:56 +0200

    Navigation and Routing Additional Techniques - Activating a Route with Code

--- Path/Angular/Angular - Getting Started/src/app/products/product-detaiul.component.ts
+++ Path/Angular/Angular - Getting Started/src/app/products/product-detaiul.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
-  selector: 'pm-product-detaiul',
   templateUrl: './product-detaiul.component.html',
   styleUrls: ['./product-detaiul.component.css']
 })
 export class ProductDetaiulComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) { }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += ` : ${id}`;
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymoous_Leaf_Rake.png"
+    }
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
